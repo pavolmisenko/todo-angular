f51507135b4a7d5d8645634318b71534d8c1d6ca
Pavol Mišenko <pavolmisenko.95@gmail.com>
2024-02-25 22:25:44 +0100

    First simple version of TODO app

---
+++ todo-angular/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css"
+})
+export class AppComponent {
+  title = "Todo app"
+
+  filter: "all" | "active" | "done" = "all"
+
+  appItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "laugh", done: false },
+    { description: "pray", done: false }
+  ]
+
+  get items() {
+    if (this.filter === "all") {
+      return this.appItems
+    }
+    return this.appItems.filter((item) => {
+      this.filter === "done" ? item.done : !item.done
+    });
+  }
+
+  addItem(description: string) {
+    this.appItems.push({
+      description,
+      done: false
+    });
+  }
+}
